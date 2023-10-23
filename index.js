d4cd63a1ee7dac876a142d9cd3546293d15fd59f
raviloi <ravi_dreamz81@yahoo.co.in>
2023-10-23 15:39:38 +0530

    Add port values and procfile

--- index.js
+++ index.js
@@ -181,8 +181,13 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-const PORT = process.env.PORT || 3000;
+
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
+
 // Start the server at Port 3000
-app.listen(PORT, function () {
-  console.log("Server started on port 3000.");
+app.listen(port, function () {
+  console.log("Server started successfully.");
 });
